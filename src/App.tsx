02d6abcd79ce9eeddcc3439eef96daaba717d8ff
iedo <iedoduarte1@gmail.com>
2026-04-09 23:06:39 -0300

    Fix GitHub Pages router basename

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { HomePage } from "@/components/HomePage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:category/:id" element={<AlgorithmView />} />
